55c1393d834ab96d0c6c2279cf779023ee822979
Thomas Ruggeri <thomas.ruggeri@snagajob.com>
2018-10-23 14:10:52 -0400

    Adding '/solve' post to api.

--- src/api.rb
+++ src/api.rb
@@ -3,3 +3,13 @@ require 'sinatra'
 get '/ok' do
   [200, 'ok']
 end
+
+post '/solve' do
+  begin
+    request.body.rewind
+    data = JSON.parse request.body.read
+    data['puzzle']
+  rescue
+    [400, 'Failure to parse request.']
+  end
+end
