6968bd97836927f0a047204bd3d33ca6b94dda42
Thomas Ruggeri <thomas.ruggeri@snagajob.com>
2018-10-23 10:28:35 -0400

    Adding Sukoku print functionallity

--- src/sudoku.rb
+++ src/sudoku.rb
@@ -22,6 +22,17 @@ class Sudoku
     end
   end
 
+  def print()
+    [0,1,2,3,4,5,6,7,8].each do |r|
+      line = ""
+      [0,1,2,3,4,5,6,7,8].each do |c|
+        elem = get_element(r, c)
+        line = "#{line} #{elem > 0 ? elem : '-'}"
+      end
+      puts line
+    end
+  end
+
   def find_possibilities
     row = 0
     poss = []
