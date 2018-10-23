09e6d049d9925747e893dba755b1bb78359f4d1d
Thomas Ruggeri <thomas.ruggeri@snagajob.com>
2018-10-23 11:15:51 -0400

    Moving solution techniques to methods.

--- src/solver.rb
+++ src/solver.rb
@@ -16,6 +16,28 @@ def print_puzzle_poss(puzzle)
   end
 end
 
+def find_only_one_poss(puzzle)
+  no_more_easy_flag = false
+  until no_more_easy_flag
+    no_more_easy_flag = true
+    (0..8).each do |r|
+      (0..8).each do |c|
+        if puzzle.get_element(r, c) > 0
+          next
+        end
+        pos = puzzle.get_poss(r, c)
+        if pos != nil and pos.length == 1
+          puzzle.set_element(r, c, pos[0])
+          no_more_easy_flag = false
+        end
+      end
+    end
+  end
+end
+
+
+
+
 puts 'Please provide the puzzle file:'
 input_file = gets
 input = read_input_puzzle_from_file(input_file.chomp)
@@ -25,21 +47,6 @@ puzzle = Sudoku.new(input)
 puts '--- Possiblilites ---'
 print_puzzle_poss(puzzle)
 
-no_more_easy_flag = false
-until no_more_easy_flag
-  no_more_easy_flag = true
-  (0..8).each do |r|
-    (0..8).each do |c|
-      if puzzle.get_element(r, c) > 0
-        next
-      end
-      pos = puzzle.get_poss(r, c)
-      if pos != nil and pos.length == 1
-        puzzle.set_element(r, c, pos[0])
-        no_more_easy_flag = false
-      end
-    end
-  end
-end
+find_only_one_poss(puzzle)
 
 puzzle.print
