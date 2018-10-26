8c338cb341a401bb4296da8463d3120c20baddb7
Thomas Ruggeri <truggeri@gmail.com>
2018-10-26 00:06:40 -0400

    Tiny refactor of run_local

--- src/run_local.rb
+++ src/run_local.rb
@@ -2,7 +2,7 @@ require_relative 'file_input'
 require_relative 'solver'
 
 puts 'Please provide the puzzle file:'
-input_file = gets
+input_file = gets.chomp
 
-puzzle_solver = Solver.new(read_input_puzzle_from_file(input_file.chomp))
+puzzle_solver = Solver.new(read_input_puzzle_from_file(input_file))
 puzzle_solver.solve()
