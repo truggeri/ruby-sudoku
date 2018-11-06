b61f32ad100a8100a0d8faebce261d9e389d21b6
Thomas Ruggeri <thomas.ruggeri@snagajob.com>
2018-11-06 15:49:30 -0500

    Refactor for readibility.

--- lib/sudoku/puzzle.rb
+++ lib/sudoku/puzzle.rb
@@ -21,17 +21,36 @@ module Sudoku
       poss
     end
 
-    def find_element_possibilities(row, col)
-      if @puzzle[row][col] == 0
-        find_row_poss(row, col) & find_col_poss(row, col) & find_cube_poss(row, col)
-      else
-        [@puzzle[row][col]]
+    def each_row(exclude: nil, &block)
+      (0..8).each do |c|
+        next if c == exclude
+        block.call(c)
       end
+    end
+
+    def each_column(&block)
+      (0..8).each do |r|
+        block.call(r)
+      end
+    end
+    
+    def each_cube(&block)
+      (0..2).each do |r|
+        (0..2).each do |c|
+          block.call(r, c)
+        end
+      end
+    end
+
+    def find_element_possibilities(row, col)
+      return [@puzzle[row][col]] if @puzzle[row][col] != 0
+      find_row_poss(row, col) & find_col_poss(row, col) & find_cube_poss(row, col)
     end
 
     def find_row_poss(row, col)
       poss =* (1..9)
-      @puzzle[row].each do |val|
+      each_column do |c|
+        val = @puzzle[row][c]
         poss = poss - [val] unless val == 0
       end
       poss
@@ -39,7 +58,7 @@ module Sudoku
 
     def find_other_row_poss(row, col)
       poss = []
-      (0..8).each do |c|
+      each_column do |c|
         next if c == col
         poss = poss | find_element_possibilities(row, c)
       end
@@ -48,7 +67,7 @@ module Sudoku
 
     def find_col_poss(row, col)
       poss =* (1..9)
-      (0..8).each do |r|
+      each_row(exclude: row) do |r|
         val = @puzzle[r][col]
         poss = poss - [val] unless val == 0
       end
@@ -57,7 +76,7 @@ module Sudoku
 
     def find_other_col_poss(row, col)
       poss = []
-      (0..8).each do |r|
+      each_row do |r|
         next if r == row
         poss = poss | find_element_possibilities(r, col)
       end
@@ -68,11 +87,9 @@ module Sudoku
       poss =* (1..9)
       row_offset = row/3 * 3
       col_offset = col/3 * 3
-      (0..2).each do |r|
-        (0..2).each do |c|
-          val = @puzzle[r + row_offset][c + col_offset]
-          poss = poss - [val] unless val == 0
-        end
+      each_cube do |r, c|
+        val = @puzzle[r + row_offset][c + col_offset]
+        poss = poss - [val] unless val == 0
       end
       poss
     end
@@ -81,11 +98,9 @@ module Sudoku
       poss = []
       row_offset = row/3 * 3
       col_offset = col/3 * 3
-      (0..2).each do |r|
-        (0..2).each do |c|
-          next if r + row_offset == row and c + col_offset == col
-          poss = poss | find_element_possibilities(r + row_offset, c + col_offset)
-        end
+      each_cube do |r, c|
+        next if r + row_offset == row and c + col_offset == col
+        poss = poss | find_element_possibilities(r + row_offset, c + col_offset)
       end
       poss
     end
@@ -109,13 +124,13 @@ module Sudoku
     end
 
     def update_possibilities_across_row(row)
-      (0..8).each do |c|
+      each_column do |c|
         @possibilities[row][c] = find_element_possibilities(row, c)
       end
     end
 
     def update_possibilities_across_col(col)
-      (0..8).each do |r|
+      each_row do |r|
         @possibilities[r][col] = find_element_possibilities(r, col)
       end
     end
@@ -123,10 +138,8 @@ module Sudoku
     def update_possibilities_across_cube(row, col)
       row_offset = row/3 * 3
       col_offset = col/3 * 3
-      (0..2).each do |r|
-        (0..2).each do |c|
-          @possibilities[r][c] = find_element_possibilities(r, c)
-        end
+      each_cube do |r, c|
+        @possibilities[r][c] = find_element_possibilities(r, c)
       end
     end
 
@@ -136,10 +149,10 @@ module Sudoku
 
     def to_s
       output = ""
-      (0..8).each do |r|
+      each_row do |r|
         output = append_line(output) if r % 3 == 0
         line = "|"
-        (0..8).each do |c|
+        each_column do |c|
           elem = get_element(r, c)
           line = "#{line} #{elem > 0 ? elem : '-'}#{(c+1) % 3 == 0 ? ' |': ''}"
         end
