ab83d49250fa65ffcb581da79a64a22f04cc3adf
Thomas Ruggeri <thomas.ruggeri@snagajob.com>
2018-11-06 18:27:51 -0500

    Using Puzzle each method in Solver.

--- lib/sudoku/solver.rb
+++ lib/sudoku/solver.rb
@@ -29,15 +29,13 @@ module Sudoku
       no_more_easy_flag = false
       until no_more_easy_flag
         no_more_easy_flag = true
-        (0..8).each do |r|
-          (0..8).each do |c|
-            next if puzzle.get_element(r, c) > 0
-            pos = puzzle.get_possibilities(r, c)
-            if pos != nil and pos.length == 1
-              puzzle.set_element(r, c, pos[0])
-              no_more_easy_flag = false
-              overall_flag = true
-            end
+        puzzle.each do |r, c|
+          next if puzzle.get_element(r, c) > 0
+          pos = puzzle.get_possibilities(r, c)
+          if pos != nil and pos.length == 1
+            puzzle.set_element(r, c, pos[0])
+            no_more_easy_flag = false
+            overall_flag = true
           end
         end
       end
@@ -45,21 +43,19 @@ module Sudoku
     end
 
     def find_only_poss_in_dimension(puzzle, dimension=:row)
-      (0..8).each do |r|
-        (0..8).each do |c|
-          next if puzzle.get_element(r, c) > 0
-          pos = puzzle.get_possibilities(r, c)
-          others = case dimension
-            when :col then puzzle.find_other_col_poss(r, c)
-            when :cube then puzzle.find_other_cube_poss(r, c)
-            else puzzle.find_other_row_poss(r, c)
-            end
-          options = pos - others
-          if options.length == 1
-            puzzle.set_element(r, c, options[0])
-            puts "(#{r}, #{c} - #{dimension}, #{options[0]})"
-            return true
+      puzzle.each do |r, c|
+        next if puzzle.get_element(r, c) > 0
+        pos = puzzle.get_possibilities(r, c)
+        others = case dimension
+          when :col then puzzle.find_other_col_poss(r, c)
+          when :cube then puzzle.find_other_cube_poss(r, c)
+          else puzzle.find_other_row_poss(r, c)
           end
+        options = pos - others
+        if options.length == 1
+          puzzle.set_element(r, c, options[0])
+          puts "(#{r}, #{c} - #{dimension}, #{options[0]})"
+          return true
         end
       end
       false
