75a8aeb30561ba5a8dd46cae7e3eda7abc98c4c5
Thomas Ruggeri <truggeri@gmail.com>
2019-09-08 19:20:30 -0400

    Stylistic cleanup of solver

--- lib/sudoku/solver.rb
+++ lib/sudoku/solver.rb
@@ -4,57 +4,59 @@ module Sudoku
   class Solver
 
     def initialize(input)
-      @input = input
-      @puzzle = Puzzle.new(@input)
-      # puts @puzzle
+      @puzzle = Puzzle.new(input)
     end
 
     def solve
-      loops = 0
       no_updates = false
       until no_updates
-        loops += 1
-        next if find_only_option()
+        next if find_only_option
         next if find_only_poss_in_dimension(:cube)
         next if find_only_poss_in_dimension(:row)
         next if find_only_poss_in_dimension(:col)
+
         no_updates = true
       end
-      @puzzle
+      puzzle
     end
 
     def find_only_option
-      @puzzle.each do |r, c|
-        next if @puzzle.get_element(r, c) > 0
-        pos = @puzzle.get_possibilities(r, c)
-        if pos != nil and pos.length == 1
-          @puzzle.set_element(r, c, pos[0])
+      puzzle.each do |r, c|
+        next if puzzle.get_element(r, c).positive?
+
+        pos = puzzle.get_possibilities(r, c)
+        if !pos.nil? && pos.length == 1
+          puzzle.set_element(r, c, pos[0])
           return true
         end
       end
       false
     end
 
-    def find_only_poss_in_dimension(dimension=:row)
-      @puzzle.each do |r, c|
-        next if @puzzle.get_element(r, c) > 0
-        options = @puzzle.get_possibilities(r, c) - find_other_poss(dimension, r, c)
+    def find_only_poss_in_dimension(dimension = :row)
+      puzzle.each do |r, c|
+        next if puzzle.get_element(r, c).positive?
+
+        options = puzzle.get_possibilities(r, c) - find_other_poss(dimension, r, c)
         if options.length == 1
-          @puzzle.set_element(r, c, options[0])
-          # puts "(#{r}, #{c} - #{dimension}, #{options[0]})"
+          puzzle.set_element(r, c, options.first)
           return true
         end
       end
       false
     end
 
-    def find_other_poss(dimension, r, c)
-      return case dimension
-        when :row then @puzzle.find_other_row_poss(r, c)
-        when :col then @puzzle.find_other_col_poss(r, c)
-        when :cube then @puzzle.find_other_cube_poss(r, c)
-        else []
-        end
+    def find_other_poss(dimension, row, col)
+      case dimension
+      when :row  then puzzle.find_other_row_poss(row, col)
+      when :col  then puzzle.find_other_col_poss(row, col)
+      when :cube then puzzle.find_other_cube_poss(row, col)
+      else []
+      end
     end
+
+    private
+
+    attr_reader :puzzle
   end
 end
